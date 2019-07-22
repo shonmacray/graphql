e9b9e7db779ef5cbda178abdc068bba119e37620
shon macray <shonmacray@gmail.com>
2019-07-22 15:28:21 +0300

    added heroku port

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 let express = require("express")
-
+const PORT = process.env.PORT || 5000
 
 let app = express()
 
-app.listen("https://shon1.herokuapp.com/", ()=> console.log("server"))
+app.get("/", (req, res)=> {
+    res.send("jejejejeje")
+})
+
+app.listen(PORT, ()=> console.log("server"))
